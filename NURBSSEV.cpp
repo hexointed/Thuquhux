8fc92a591002613c1630f1ca949235ff73e3203d
Elias Forsberg 'hexointed <forsberg.elias@gmail.com>
2013-04-28 16:41:53 +0200

    Renamed NURBS_Surface to NURBSSEV (NURBS Surface Encapsulated Volume)

---
+++ NURBSSEV.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   NURBS.cpp
+ * Author: elias
+ * 
+ * Created on April 24, 2013, 4:42 PM
+ */
+
+#include "NURBSSEV.h"
+
+NURBSSEV::NURBSSEV() {
+}
+
+NURBSSEV::NURBSSEV(const NURBSSEV& orig) {
+}
+
+NURBSSEV::~NURBSSEV() {
+}
+
---
+++ NURBSSEV.h
@@ -0,0 +1,46 @@
+/* 
+ * File:   NURBS.h
+ * Author: elias
+ *
+ * Created on April 24, 2013, 4:42 PM
+ */
+
+#ifndef NURBSSEV_H
+#define	NURBSSEV_H
+
+class NURBSSEV {    //Non Uniform Rational B-Spline Surface Encapsulated Volume
+public:
+    NURBSSEV();
+    NURBSSEV(const NURBSSEV& orig);
+    virtual ~NURBSSEV();
+    
+    NURBSSEV Unite(NURBSSEV a);
+    NURBSSEV Intersect(NURBSSEV a);
+    NURBSSEV Complement(NURBSSEV a);
+    NURBSSEV Differatiate(NURBSSEV a);
+    
+    NURBSSEV& operator=(NURBSSEV v) const;
+    
+    bool operator==(const NURBSSEV& v) const;
+    bool operator!=(const NURBSSEV& v) const;
+    bool operator< (const NURBSSEV& v) const;
+    bool operator> (const NURBSSEV& v) const;
+    bool operator<=(const NURBSSEV& v) const;
+    bool operator>=(const NURBSSEV& v) const;
+    
+private:
+    void re_calculate_mesh();
+    
+public:
+    double (*mesh_vertecies)[3];
+    
+private:
+    double * knots;
+    double (*ctrlpoints)[3];
+    double * weights;
+    int ctrlpts_width;
+    int ctrlpts_depth;
+};
+
+#endif	/* NURBSSEV_H */
+
