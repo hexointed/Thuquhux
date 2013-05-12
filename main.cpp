b09e295e2f54390b61512a0076c7ee8967c921cc
Elias Forsberg <forsberg.elias@gmail.com>
2013-05-12 17:02:36 +0200

    Added a fragment shader, renamed vertex shader file

--- main.cpp
+++ main.cpp
@@ -242,23 +242,44 @@ void InitGlut(int argc, char **argv){
 }
 
 void InitShader(){
-	std::ifstream in("Shaders/test.vshader");
-	std::string str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
-	char * prgrm = new char[str.size()];
-	strcpy(prgrm, str.c_str());
+	{
+		std::ifstream in("Shaders/vertex.arbvs");
+		std::string str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+		char * prgrm = new char[str.size()];
+		strcpy(prgrm, str.c_str());
 	
-	glEnable(GL_VERTEX_PROGRAM_ARB);
-	unsigned int handle[1];
-	glGenProgramsARB(1,handle);
-	glBindProgramARB(GL_VERTEX_PROGRAM_ARB, *handle);
-	glProgramStringARB(GL_VERTEX_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB, str.size(), &prgrm[0]);
+		glEnable(GL_VERTEX_PROGRAM_ARB);
+		unsigned int handle[1];
+		glGenProgramsARB(1,handle);
+		glBindProgramARB(GL_VERTEX_PROGRAM_ARB, *handle);
+		glProgramStringARB(GL_VERTEX_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB, str.size(), &prgrm[0]);
 	
-	int err;
-	glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &err);
-	if(err !=-1){
-		std::string error = (const char*) glGetString(GL_PROGRAM_ERROR_STRING_ARB);
-		std::cout<<"Vertex program failed:\n"<<err<<std::endl<<error<<std::endl;
+		int err;
+		glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &err);
+		if(err !=-1){
+			std::string error = (const char*) glGetString(GL_PROGRAM_ERROR_STRING_ARB);
+			std::cout<<"Vertex program failed:\n"<<err<<std::endl<<error<<std::endl;
+		}
+		delete[] prgrm;
 	}
+	{
+		std::ifstream in("Shaders/fragment.arbfs");
+		std::string str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+		char * prgrm = new char[str.size()];
+		strcpy(prgrm, str.c_str());
 	
-	delete[] prgrm;
+		glEnable(GL_FRAGMENT_PROGRAM_ARB);
+		unsigned int handle[1];
+		glGenProgramsARB(1,handle);
+		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, *handle);
+		glProgramStringARB(GL_FRAGMENT_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB, str.size(), &prgrm[0]);
+	
+		int err;
+		glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &err);
+		if(err !=-1){
+			std::string error = (const char*) glGetString(GL_PROGRAM_ERROR_STRING_ARB);
+			std::cout<<"Vertex program failed:\n"<<err<<std::endl<<error<<std::endl;
+		}
+		delete[] prgrm;
+	}
 }
