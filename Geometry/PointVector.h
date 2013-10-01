250e97514c0496848a84ea8d2fb32fc38ad14271
Elias Forsberg <forsberg.elias@gmail.com>
2013-10-01 14:13:26 +0200

    Added comments to PointVector.h The PointVector class is now mostly complete

--- Geometry/PointVector.h
+++ Geometry/PointVector.h
@@ -3,6 +3,9 @@
  * Author: Elias Forsberg
  *
  * Created on December 26, 2012, 4:19 PM
+ * 
+ * This class is hoped to be useful for representing N-dimensional points and 
+ * vectors. It includes several arithmetic operations that are common for vectors.
  */
 
 #ifndef POINTVECTOR_H
@@ -24,6 +27,9 @@ public:
 	PointVector(Tail... t);
 
 private:
+	/*
+	 *  Constructor helper functions (for the variadic template constructor)
+	 */
 	template<typename First, typename... Tail>
 	inline void pconstruct(int i, First f, Tail... t);
 	inline void pconstruct(int i);
@@ -32,6 +38,10 @@ private:
 	double comp[Dim];
     
 public:
+	/*
+	 * Returns composants of the PointVector<Dim>. 
+	 * x,y and z represent comp[0], comp[1] and comp[2], respectively. 
+	 */
 	double get(int i) const;
 	double getdx() const;
 	double getdy() const;
@@ -44,6 +54,11 @@ public:
 	void setdx(double d);
 	void setdy(double d);
 	void setdz(double d);
+	
+	/*
+	 * Some common arithmetic operations. There exist several versions of many
+	 * of these operations; as operators, member methods, and static methods.
+	 */
 	
 	inline PointVector operator +(const PointVector r) const;
 	inline PointVector operator -(const PointVector r) const;
@@ -84,6 +99,11 @@ public:
 	static PointVector pow(double d, PointVector p);
 	static PointVector pow_comp(PointVector p, PointVector q);
 	
+	/*
+	 * a is considered max_comp(b) if all components of a are equal or bigger
+	 * than their respective component in b
+	 */
+	
 	PointVector& set_min_comp(PointVector p);
 	PointVector& set_max_comp(PointVector p);
 	static PointVector min_comp(PointVector p, PointVector q);
@@ -93,6 +113,11 @@ public:
 	bool is_max_comp(PointVector p) const;
 	bool is_eq_comp (PointVector p) const;
 	
+	/*
+	 * A unit vector is a vector with the magnitude 1. make_unit() and 
+	 * make_unit(p) scale down their vectors so that their magnitude is one,
+	 * but the relative sizes of the composants remain.
+	 */
 	PointVector& make_unit();
 	static PointVector make_unit(PointVector p);
 	
