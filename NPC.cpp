f27db4fe50efe92e8c396ca90706a5468a15d7e4
Joline Granath <joline.granath@gmail.com>
2013-10-08 14:13:20 +0200

    improved updatePosition()

--- NPC.cpp
+++ NPC.cpp
@@ -3,12 +3,13 @@
 #include <time.h>
 #include "PhysObject.h"
 #include <vector>
+#include <iostream>
 
 PointVector<> PLACEHOLDER_PLAYER_POSITION;
 std::vector<PointVector<>> PLACEHOLDER_LINE_OF_TRAVEL;
 PointVector<> GRAVITY_CENTER;
 
-double NPC::getPosition{
+PointVector<> NPC::getPosition(){
 
 	for(;;){
 		return PLACEHOLDER_PLAYER_POSITION;
@@ -18,7 +19,7 @@ double NPC::getPosition{
 
 
 
-double NPC::makePosition{
+void NPC::makePosition(){
 	
 	srand(time(0));
 	position.getdx = rand();
@@ -35,81 +36,120 @@ for( int i = 0; i < 1000000; ++i )
 }
 }
 
+double NPC::climbingAbilityx(){
+	srand ( time(NULL) );
+	climbingAbilityx = rand() % 11;
+	
+	return climbingAbilityx;
+}
+
+
+double NPC::climbingAbilityy(){
+	srand ( time(NULL) );
+	climbingAbilityy = rand() % 11;
+	
+	return climbingAbilityy;
+}
+
+
+double NPC::climbingAbilityz(){
+	srand (time(NULL));
+	climbingAbilityz = rand() % 11;
+	
+	return climbingAbilityz;
+}
+
+bool NPC::randomness(){
+	srand (time(NULL));
+	int randomInt = rand() % 50;
+	
+	randomness = randomInt == 2 ?  1 : 0;
+}
+
+
 double NPC::updatePosition(deltaT){
 
-	if(GRAVITY_CENTER.getdx() > position.getdx()){
-		climbingAbilityx = INFINITY;
-	}
-
-	if(GRAVITY_CENTER.getdy() > position.getdy()){
-		climbingAbilityy = INFINITY;
-	}
-
-	if(GRAVITY_CENTER.getdz() > position.getdz()){
-		climbingAbilityz = INFINITY;
-	}
-
-
-	int k{
-		if(randomBool == 1){
-			k=1;
-		}
-
-		if(randomBool == 0){
-			k=(-1);
-		}
-	}
+	double infinity = 1.0/0.0;
 
 	double sx = deltaT*velocityx;
 	double sy = deltaT*velocityy;
 	double sz = deltaT*velocityz;
 
-	if(position.getdx() < PLACEHOLDER_LINE_OF_TRAVEL.getdx()){
 
-		if(PLACEHOLDER_LINE_OF_TRAVEL.getdx() - (position.getdx() + k*sx) >= 0){
-			position.setdx(position.getdx() + k*sx);
-		}
-		else{
-			position.setdx(PLACEHOLDER_LINE_OF_TRAVEL.getdx());
-		}
+	if(random == 1){
+		static double b;
+		konstant double a = position.getdx();		
+		position.setdx(a+sin(b));
 	}
+	
+	else{
+		int k = randomBool == 1 ? 1 : -1;
 
-	if(position.getdy() < PLACEHOLDER_LINE_OF_TRAVEL.getdy()){
-		if(PLACEHOLDER_LINE_OF_TRAVEL.getdy() - (position.getdy() + k*sy) >= 0){
-			position.setdy(position.getdy() + k*sy);
-		}	
-		else{
-			position.setdy(PLACEHOLDER_LINE_OF_TRAVEL.getdy())
+		if(GRAVITY_CENTER.getdx() > position.getdx()){
+			climbingAbilityx = infinity;
+		}
+	
+		if(GRAVITY_CENTER.getdy() > position.getdy()){
+			climbingAbilityy = infinity;
 		}
 
-	}
 
-	if(position.getdz() < PLACEHOLDER_LINE_OF_TRAVEL.getdz()){
-		if(PLACEHOLDER_LINE_OF_TRAVEL.getdz() - (position.getdz() + k*sz) >= 0){
-			position.setdz(position.getdz() + k*sz);
+		if(GRAVITY_CENTER.getdz() > position.getdz()){
+			climbingAbilityz = infinity;
 		}
-		else{
-			position.setdz(PLACEHOLDER_LINE_OF_TRAVEL.getdz());
+
+
+
+		if(position.getdx() < PLACEHOLDER_LINE_OF_TRAVEL.getdx()){
+			if(sx<climbingAbilityx){
+
+				if((PLACEHOLDER_LINE_OF_TRAVEL.getdx() - (position.getdx() + k*sx)) >= 0){
+					position.setdx(position.getdx() + k*sx);
+				}
+
+				else{
+					position.setdx(PLACEHOLDER_LINE_OF_TRAVEL.getdx());
+				}
+			}
+		
+			else{
+				position.setdx(position.getdx() - k*sx);	
+			}
 		}
-	}
 
+		if(position.getdy() < PLACEHOLDER_LINE_OF_TRAVEL.getdy()){
+			if(sy<climbingAbilityy){
+		
+				if(PLACEHOLDER_LINE_OF_TRAVEL.getdy() - (position.getdy() + k*sy) >= 0){
+					position.setdy(position.getdy() + k*sy);
+				}	
+				else{
+					position.setdy(PLACEHOLDER_LINE_OF_TRAVEL.getdy())
+				}
+			}
+		
+			else{
+					position.setdy(position.getdy() - k*sy,);	
+			}
+		}
+
+		if(position.getdz() < PLACEHOLDER_LINE_OF_TRAVEL.getdz()){
+			if(sz<climbingAbilityz){
+			
+				if(PLACEHOLDER_LINE_OF_TRAVEL.getdz() - (position.getdz() + k*sz) >= 0){
+					position.setdz(position.getdz() + k*sz);
+				}	
+				else{
+					position.setdz(PLACEHOLDER_LINE_OF_TRAVEL.getdz())
+				}
+			}
+			
+			else{
+					position.setdz(position.getdz() - k*sz,);	
+			}
+		}
+}	
 }
-
-
-
-
-if(GRAVITY_CENTER.getdx() > position.getdx()){
-	climbingAbilityx = INFINITY;
-}
-
-if(GRAVITY_CENTER.getdy() > position.getdy()){
-	climbingAbilityy = INFINITY;
-}
-
-if(GRAVITY_CENTER.getdz() > position.getdz()){
-	climbingAbilityz = INFINITY;
-}
-
 
 
 
--- NPC.h
+++ NPC.h
@@ -9,15 +9,16 @@ private:
 
 	PointVector<> position;
 
-	bool randomBool;
+	bool randomBool(); /*NPC moving towards or away from player*/
+	bool randomness(); /*For totally random moving NPCs*/
 
 	double velocityx;
 	double velocityy;
 	double velocityz;
 
-	double climbingAbilityx;
-	double climbingAbilityy;
-	double climbingAbilityz;
+	double climbingAbilityx();
+	double climbingAbilityy();
+	double climbingAbilityz();
 
 	double getPosition(); 
 	double makePosition();
