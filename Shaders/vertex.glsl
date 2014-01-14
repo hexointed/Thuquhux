63028d9cf8ab6ef17a168cf99397e1097e2bee1e
Elias Forsberg <forsberg.elias@gmail.com>
2014-01-14 09:56:45 +0100

    fixed Opengl crash

--- Shaders/vertex.glsl
+++ Shaders/vertex.glsl
@@ -3,12 +3,12 @@
 //varying vec3 lightPos;
 
 void main(void){
-	vec3 lightPos = {0,1,5};
-	float lightRadius = 5;
+	vec3 lightPos = vec3(0.0, 1.0, 5.0);
+	float lightRadius = 5.0;
 	gl_Position = ftransform();
-	vec4 col = {0.5,1,0.7,0};
+	vec4 col = vec4(0.5, 1.0, 0.7, 0.0);
 	float dist = distance(gl_Position.xyz, lightPos);
 	
-	float denom = (dist/lightRadius + 1);
-	gl_FrontColor = (gl_Color * col * 2)/(denom*denom);
+	float denom = (dist/lightRadius + 1.0);
+	gl_FrontColor = (gl_Color * col * 2.0)/(denom*denom);
 }
