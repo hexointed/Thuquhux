adc7d5610550c1a54147925ea63cd7913ae1726f
Elias Forsberg <forsberg.elias@gmail.com>
2014-08-11 15:59:21 +0200

    fixed random::uniform

--- Utility/Random.hpp
+++ Utility/Random.hpp
@@ -5,8 +5,8 @@
  * Created on August 8, 2014, 10:15
  */
 
-#ifndef UTILITY_RANDOM
-#define UTILITY_RANDOM
+#ifndef UTILITY_RANDOM_HPP
+#define UTILITY_RANDOM_HPP
 
 template<typename T>
 T Random::uniform(T min, T max){
@@ -14,15 +14,9 @@ T Random::uniform(T min, T max){
 	return r_dist(gen);
 }
 
-template<>
-int Random::unifom(int min, int max){
-	std::uniform_int_distribution<> i_dist(min, max);
-	return i_dist(gen);
-}
-
 template<typename T>
 T Random::normal(T mean, T stddev){
 	return n_dist(gen) * stddev + mean;
 }
 
-#endif /* UTILITY_RANDOM */
+#endif /* UTILITY_RANDOM_HPP */
